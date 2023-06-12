822c85d66f88b8ba1785221884abe5d7e2f92c91
MyeongJun Park <myeong7712@gmail.com>
2023-06-12 09:02:34 +0900

    바뀐내용 없음..

--- src/main/java/com/multi/racket/controller/BulletinBoardController.java
+++ src/main/java/com/multi/racket/controller/BulletinBoardController.java
@@ -1,8 +1,8 @@
 package com.multi.racket.controller;
 
 import org.springframework.stereotype.Controller;
+
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
